d816b8e2a9e0bd3e933f943c97290290e6f7cbc2
lucas <lucasmacielaguiar@gmail.com>
2019-06-10 23:39:59 -0300

    implement breadth_search

--- grafo.py
+++ grafo.py
@@ -3,37 +3,57 @@ class Object:
         self.value = value
         self.edges = []
         self.visited = False
+
     def add_edge(self, object):
         self.edges.append(object)
+
+
 class Graph:
     def __init__(self, array_objects=[]):
         self.graph = []
         self.graph = array_objects
+
     def breadth_search(self):
-        node = self.graph[0]
-        node.visited = True
         queue = []  # cria fila de execucao
+
         def enqueue(node):
-            node.visited = True
             queue.append(node)  # adiciona no na fila
+
         def dequeue():
             return queue.pop(0)
-        enqueue(node)
-        while len(queue) != 0:
-            u = dequeue()  # remove primeiro da fila
-            for v in u.edges:
-                if v.visited == False:
-                    enqueue(v)
+        for node in self.graph:
+            if node.visited == False:
+                enqueue(node)
+                print(node.value)
+                node.visited = True
+                while len(queue) != 0:
+                    u = dequeue()  # remove primeiro da fila
+                    for v in u.edges:
+                        if v.visited == False:
+                            print(v.value)
+                            v.visited = True
+                            enqueue(v)
 
 
 def main():
-    array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    node1 = Object(array[0])
-    node2 = Object(array[5])
-    node3 = Object(array[2])
+    node1 = Object(1)
+    node3 = Object(3)
+    node6 = Object(6)
+    node8 = Object(8)
+    node9 = Object(9)
+    node7 = Object(7)
+    node5 = Object(5)
 
-    node1.add_edge(node2)
     node1.add_edge(node3)
-    print(node1.edges[0].value)
+    node1.add_edge(node6)
+
+    node3.add_edge(node8)
+
+    node6.add_edge(node9)
+    node6.add_edge(node7)
+
+    graph = Graph([node1, node3, node6, node8, node9, node7, node5])
+    graph.breadth_search()
+
 
 main()
