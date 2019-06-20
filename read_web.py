6082de430c97e7fefe4de6b5f244e6d0b4d6f7f7
lucas <lucasmacielaguiar@gmail.com>
2019-06-20 16:03:39 -0300

    add get related url pages

---
+++ read_web.py
@@ -0,0 +1,30 @@
+import urllib
+from urllib import request
+import re
+
+
+def getPageHtml(url='https://'):
+    '''
+        funcao para retornar conteudo html da url passada
+    '''
+    try:
+        yTUBE = urllib.request.urlopen(url).read()
+        return str(yTUBE)
+    except urllib.error.URLError as e:
+        print(e.reason)
+        exit(1)
+
+
+def related_pages(content):
+    url_match = '"https?://\S+"'
+    regex = re.compile(r'' + url_match)
+    result = re.findall(regex, str(content))
+    for index in range(len(result)):
+        result[index] = ''.join(result[index].split("\""))
+    return result
+
+
+link = "http://www.unb.br/"
+content = getPageHtml(link)
+result = related_pages(content)
+print(result)
